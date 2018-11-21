bbe4ecc506c50cf5a96948ad631a0ecf12503a5d
Jesse Stone <jstone@psi.edu>
2018-11-21 08:47:33 -0700

    Now uses a list of label tuples, instead of a map of label files

--- process_uploads.py
+++ process_uploads.py
@@ -6,86 +6,100 @@ import os.path
 import itertools
 from bs4 import BeautifulSoup
 
+IGNORE_FILES=['signature.md5', '.autoxfer']
+
 def main(argv=None):
     if argv == None:
         argv = sys.argv
 
     dirname = argv[1]
-
-    if semaphore_exists(dirname):
-        process_uploads(dirname, '')
-
-        #update_archive(archive_dir, changes)
+    process_directory(dirname)
 
     return 0
+
+def process_directory(dirname)
+    files = [x for x in find_files(dirname)]
+    if semaphore_exists(dirname, files):
+        process_uploads(dirname, files,'')
+
+        #update_archive(archive_dir, changes)
 
 def update_archive(archive_dir, changes):
     pass
 
-def semaphore_exists(dirname):
-    return True
+def semaphore_exists(dirname, files):
+    return os.path.join(dirname, '.autoxfer') in files
 
-def process_uploads(dirname, archive_dir):
+def process_uploads(dirname, files, archive_dir):
     #for each label in label directory:
     #   parse the label, yielding product id and file name
     #   move label and file to destination directory
     #   add an entry to the change list for that product
-    files = [x for x in find_files(dirname)]
-    labeldict = parselabels(dirname, files)
+    labels = parselabels(dirname, files)
 
-    label_files = labeldict.keys()
-    data_files = [get_sibling(l, labeldict.get(l, {})['file_name']) for l in labeldict.keys()]
+    label_files = [l['label_file_name'] for l in labels]
+    data_files = [l['file_name'] for l in labels]
 
-    unaccounted = [f for f in files if f not in label_files and f not in data_files]
+    unaccounted = [f for f in files if f not in label_files and f not in data_files and not ignore(f)]
 
     if unaccounted:
        raise (Exception('Some files were not recognized as products: ' + ','.join(unaccounted)))
 
 
     # move the files to the archive directory
-    for f in label_files.keys():
-        l = label_files[f]
+    for l in labels:
         collection_id = l['collection_id']
-        dest_directory = os.path.join(dir_name, collection_id)
+        dest_directory = os.path.join(dirname, collection_id)
         # move file
 
 
-    lids = [(label_files[f]['collection_id'], label_files[f]['logical_id']) for f in label_files.keys()]
+    lids = [(l['collection_id'], l['logical_id']) for l in labels]
 
-    collection_produts = {}
+
+    collection_products = {}
     for collection_id, logical_id in lids:
-        collection_products.setdefault(collectionId, []).append(logical_id)
+        collection_products.setdefault(collection_id, []).append(logical_id)
+
+    lidvids = [l['logical_id'] + "::" + l['version_id'] for l in labels]
+    collection_inventory = ['P,' + lidvid for lidvid in lidvids]
+
+    print ("\r\n".join(collection_inventory))
+
 
     # update the collections
     #    increment the version number
     #    add the products in the change list to the collection inventory
+    #    need collection template
+    
 
     # update the bundle
     #    increment the version number
+    #    need bundle template
 
 def parselabels(dirname, files):
     # find all .xml files
     xmlfiles = [x for x in files if x.endswith('.xml')]
-    labelpairs = [(x, extractlabel(parselabel(x))) for x in xmlfiles]
-    return dict([(x, l) for x, l in labelpairs if l])
-
+    return [extractlabel(parselabel(x), x) for x in xmlfiles]
 
 def find_files(dirname):
     filelists = [[os.path.join(subdir,f) for f in files] for subdir, _, files in os.walk(dirname)]
     return itertools.chain.from_iterable(filelists)
 
-def extractlabel(label):
+def extractlabel(label, labelfilename):
     p = label.Product_Observational
     if p:
         result = {}
+        result['label_file_name'] = labelfilename
 
         i = p.Identification_Area
         lid = i.logical_identifier.string
         result['logical_id'] = lid
         result['collection_id'] = lid.split(':')[4]
+        result['version_id'] = i.version_id.string
 
         f = p.File_Area_Observational
         result ['file_name'] = f.File.file_name.string
+        
         
         return result
     else:
@@ -98,6 +112,10 @@ def parselabel(filename):
 def get_sibling(fq_file, f):
     dirname = os.path.dirname(fq_file)
     return os.path.join(dirname, f)
+
+def ignore(file):
+    return any((file.endswith(name) for name in IGNORE_FILES))
+
 
 if __name__ == '__main__':
     sys.exit(main())
