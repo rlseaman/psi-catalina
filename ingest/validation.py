c2214daa1972d085f366d91978f3feee982ae78d
Jesse Stone <jstone@psi.edu>
2019-02-22 13:38:09 -0700

    Added a class to run the validator

---
+++ ingest/validation.py
@@ -0,0 +1,34 @@
+import subprocess
+import os.path
+import json
+
+MODEL_VERSION='1A00'
+SCHEMA_PATH='../schemas' 
+
+IMAGING_SCHEMA=os.path.join(SCHEMA_PATH, 'PDS4_IMG_1A10.xsd')
+DISP_SCHEMA=os.path.join(SCHEMA_PATH, 'PDS4_DISP_1B00.xsd')
+GEOM_SCHEMA=os.path.join(SCHEMA_PATH, 'PDS4_GEOM_1A10.xsd')
+CATALINA_SCHEMA=os.path.join(SCHEMA_PATH, 'PDS4_CSS_1A10.xsd')
+
+IMAGING_SCHEMATRON=os.path.join(SCHEMA_PATH, 'PDS4_IMG_1A10.sch')
+DISP_SCHEMATRON=os.path.join(SCHEMA_PATH, 'PDS4_DISP_1B00.sch')
+GEOM_SCHEMATRON=os.path.join(SCHEMA_PATH, 'PDS4_GEOM_1A10.sch')
+CATALINA_SCHEMATRON=os.path.join(SCHEMA_PATH, 'PDS4_CSS_1A10.sch')
+
+
+def run_validator(file_name):
+    p = subprocess.run(['validate', 
+        '-D', 
+        '-m', MODEL_VERSION,
+        '-x', IMAGING_SCHEMA, DISP_SCHEMA, GEOM_SCHEMA,
+        '-S', IMAGING_SCHEMATRON, DISP_SCHEMATRON, GEOM_SCHEMATRON,
+        '-s', 'json'
+        '-t', file_name], stdout=subprocess.PIPE)
+    stdout = p.stdout
+    return json.loads(stdout)
+
+class Validation():
+    def init(self, dirname):
+        result = run_validator(dirname)
+        self.failures =  [x for x in result['productLevelValidationResults'] if x['status'] == "FAIL"]
+        self.successes =  [x for x in result['productLevelValidationResults'] if x['status'] == "PASS"]
