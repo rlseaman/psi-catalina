addd937817d4599239bc20c4291a1e2b4166223e
Jesse Stone <jstone@psi.edu>
2019-05-08 16:23:52 -0700

    now builds up the list of schema/schematron files from a list of names

--- ingest/validation.py
+++ ingest/validation.py
@@ -5,26 +5,22 @@ import json
 MODEL_VERSION='1A00'
 SCHEMA_PATH='../schemas' 
 
-IMAGING_SCHEMA=os.path.join(SCHEMA_PATH, 'PDS4_IMG_1A10.xsd')
-DISP_SCHEMA=os.path.join(SCHEMA_PATH, 'PDS4_DISP_1B00.xsd')
-GEOM_SCHEMA=os.path.join(SCHEMA_PATH, 'PDS4_GEOM_1A10.xsd')
-CATALINA_SCHEMA=os.path.join(SCHEMA_PATH, 'pds4_catalina.ldd_CSS_1000.xsd')
-
-IMAGING_SCHEMATRON=os.path.join(SCHEMA_PATH, 'PDS4_IMG_1A10.sch')
-DISP_SCHEMATRON=os.path.join(SCHEMA_PATH, 'PDS4_DISP_1B00.sch')
-GEOM_SCHEMATRON=os.path.join(SCHEMA_PATH, 'PDS4_GEOM_1A10.sch')
-CATALINA_SCHEMATRON=os.path.join(SCHEMA_PATH, 'pds4_catalina.ldd_CSS_1000.sch')
-
+DICTIONARIES=['PDS4_IMG_1A10_1510', 'PDS4_DISP_1B00','PDS4_GEOM_1A10','pds4_catalina.ldd_CSS_1000']
+SCHEMA_FILES=[x + '.xsd' for x in DICTIONARIES]
+SCHEMATRON_FILES=[x + '.sch' for x in DICTIONARIES]
+SCHEMA_PATHS=[os.path.join(SCHEMA_PATH, x) for x in SCHEMA_FILES]
+SCHEMATRON_PATHS=[os.path.join(SCHEMA_PATH, x) for x in SCHEMATRON_FILES]
 
 def run_validator(file_name):
     p = subprocess.run(['validate', 
         '-D', 
         '-s', 'json',
         '-m', MODEL_VERSION,
-        '-x', IMAGING_SCHEMA, DISP_SCHEMA, GEOM_SCHEMA, CATALINA_SCHEMA,
-        '-S', IMAGING_SCHEMATRON, DISP_SCHEMATRON, GEOM_SCHEMATRON, CATALINA_SCHEMATRON,
+        '-x', *SCHEMA_PATHS,
+        '-S', *SCHEMATRON_PATHS,
         '-t', file_name], stdout=subprocess.PIPE)
     stdout = p.stdout
+    print(stdout)
     return json.loads(stdout)
 
 class Validation:
