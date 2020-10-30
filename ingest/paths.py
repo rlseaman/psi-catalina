eac48578bad345360bcd833561af690eadb4478b
Jesse Stone <jstone@psi.edu>
2020-10-30 16:21:22 -0700

    added a class that calculates the file paths

---
+++ ingest/paths.py
@@ -0,0 +1,29 @@
+import os
+
+class Paths:
+    def __init__(self, basedir):
+        self.basedir = basedir
+
+    def datadir(self, inst=None, year=None, date=None, filename=None):
+        if filename:
+            return os.path.join(self.basedir, inst, year, date, filename)
+        elif date:
+            return os.path.join(self.basedir, inst, year, date)
+        elif year:
+            return os.path.join(self.basedir, inst, year)
+        elif inst:
+            return os.path.join(self.basedir, inst)
+        else:
+            return self.basedir
+
+    def labeldir(self, inst=None, year=None, date=None, filename=None):
+        if filename:
+            return os.path.join(self.basedir, inst, year, "pds4", date, filename)
+        elif date:
+            return os.path.join(self.basedir, inst, year, "pds4" ,date)
+        elif year:
+            return os.path.join(self.basedir, inst, year)
+        elif inst:
+            return os.path.join(self.basedir, inst)
+        else:
+            return self.basedir
