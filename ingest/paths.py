a30c4e2e56cf2733fcc482061cced48766adaca9
Jesse Stone <jstone@psi.edu>
2020-11-03 11:06:19 -0700

    cleaned up the path generation code.
    added some minimal tests.

--- ingest/paths.py
+++ ingest/paths.py
@@ -1,4 +1,5 @@
 import os
+import itertools
 
 class Paths:
     def __init__(self, basedir, dest):
@@ -6,28 +7,34 @@ class Paths:
         self.dest = dest
 
     def datadir(self, inst=None, year=None, date=None, filename=None):
-        if filename:
-            return os.path.join(self.basedir, inst, year, date, filename)
-        elif date:
-            return os.path.join(self.basedir, inst, year, date)
-        elif year:
-            return os.path.join(self.basedir, inst, year)
-        elif inst:
-            return os.path.join(self.basedir, inst)
-        else:
-            return self.basedir
+        return self._buildpath((self.basedir, inst, year, date, filename))
 
     def labeldir(self, inst=None, year=None, date=None, filename=None):
-        if filename:
-            return os.path.join(self.basedir, inst, year, "pds4", date, filename)
-        elif date:
-            return os.path.join(self.basedir, inst, year, "pds4" ,date)
-        elif year:
-            return os.path.join(self.basedir, inst, year)
-        elif inst:
-            return os.path.join(self.basedir, inst)
-        else:
-            return self.basedir
-
+        subdir = "pds4" if date else None
+        return self._buildpath((self.basedir, inst, year, subdir, date, filename))
+        
     def destdir(self, collection_id, inst, year, date):
-        return os.path.join(self.dest, collection_id, inst, year, date)
+        return self._buildpath((self.dest, collection_id, inst, year, date))
+
+    def _buildpath(self, elements):
+        return os.path.join(*self._filledElements(elements))
+
+    def _filledElements(self, elements):
+        elementList = list(elements)
+        if any(itertools.dropwhile(lambda x: x, elementList)):
+            raise Exception("Gaps detected in path:", elementList)
+        return list(itertools.takewhile(lambda x: x, elementList))
+
+if __name__ == '__main__':
+    p = Paths("base", "dest")
+    print(p.datadir("I52"))
+    print(p.datadir("I52", "2020"))
+    print(p.datadir("I52", "2020", "20Aug01"))
+    print(p.datadir("I52", "2020", "20Aug01", "test.fit"))
+
+    print(p.labeldir("I52"))
+    print(p.labeldir("I52", "2020"))
+    print(p.labeldir("I52", "2020", "20Aug01"))
+    print(p.labeldir("I52", "2020", "20Aug01", "test.fit"))
+
+    print(p.destdir("data","I52", "2020", "20Aug01"))
