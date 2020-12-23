12f2d6c02bb998fe2d80bd7c22902a1e6d443c1a
Jesse Stone <jstone@psi.edu>
2020-12-23 16:32:24 -0700

    updated to use argparse

--- cssdb/import_json.py
+++ cssdb/import_json.py
@@ -4,14 +4,18 @@ import sys
 
 import cssdb
 import json
+import argparse
 
 def main(argv=None):
     if argv is None:
         argv = sys.argv
 
-    file_name = argv[1]
-    extracted = json.load(file_name)
-    cssdb.write_directory(extracted, file_name)
+    parser = argparse.ArgumentParser(description='Parse a CSS JSON file and write it to the database')
+    parser.add_argument('--filename', help='The file name for the JSON file', required=True)
+    args = parser.parse_args()
+
+    extracted = json.load(args.filename)
+    cssdb.write_directory(extracted, args.filename)
 
 if __name__ == '__main__':
     sys.exit(main())
