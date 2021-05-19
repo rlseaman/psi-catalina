3a06d673b540fae5709f8268da01d0f0b00739c7
Jesse Stone <jstone@psi.edu>
2021-05-19 16:12:40 -0700

    creates the db if it doesn't already exist

--- cssdb/import.sh
+++ cssdb/import.sh
@@ -5,7 +5,11 @@ SCRIPT_DIR=`dirname $0`
 
 pushd $SCRIPT_DIR
 
-python3 extract_all.py --basedir=/data/CSS --nights=21 --outfilebase=/data/json
+if [ ! -e cssdb.sqlite ]; then
+    sqlite3 cssdb.sqlite < cssdb_schema.sql
+fi
+
+python3 extract_all.py --basedir=/data/ready/gbo.ast.catalina.survey/ --nights=21 --outfilebase=/data/json
 find /data/json -name '*.json' -exec python3 import_json.py --filename '{}' \;
 find /data/json -name '*.json' -exec trash '{}' \;
 
