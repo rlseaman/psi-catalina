53bcec389b41fb37502062ae96b6dd9a27eead5e
Jesse Stone <jstone@psi.edu>
2021-06-24 14:47:42 -0700

    no longer re-downloads the db files every time

--- cssdb/flask/build_db.sh
+++ cssdb/flask/build_db.sh
@@ -8,6 +8,12 @@ if [ -e cssdb.sqlite ]; then
 fi
 
 sqlite3 cssdb.sqlite < cssdb_schema.sql
-wget https://sbnarchive.psi.edu/pds4/surveys/catalina_sync/catalina_json.tar.gz
+
+if [ -n "$PROD" ]; then
+    echo "Downloading fresh data..."
+    rm catalina_json.tar.gz
+    wget https://sbnarchive.psi.edu/pds4/surveys/catalina_sync/catalina_json.tar.gz
+fi
+
 tar -xzf catalina_json.tar.gz
 find json -name "*.json" -exec python3 import_json.py --filename '{}' \; 
