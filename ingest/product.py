5933637f451e9c8b0ad2316279b575c05b13b3b0
Jesse Stone <jstone@psi.edu>
2023-01-05 09:23:06 -0700

    Fixed: Passing in the wrong object when extracting product_ancillary

--- ingest/product.py
+++ ingest/product.py
@@ -14,7 +14,7 @@ def extract_label(xmldoc, filepath=''):
     if xmldoc.Product_Observational:
         return label.extract_product_observational(xmldoc.Product_Observational)
     if xmldoc.Product_Ancillary:
-        return label.extract_product_ancillary(xmldoc.Product_Observational)
+        return label.extract_product_ancillary(xmldoc.Product_Ancillary)
     if xmldoc.Product_Document:
         return label.extract_product_document(xmldoc.Product_Document)
 
